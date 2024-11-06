2c8f1ab2cd1aaff805ff154d7e77a76fd93b0482
iWaywell <u03iw22@abdn.ac.uk>
2024-11-06 19:55:12 +0000

    added functionality to generate random
    Lots of information still needed to get from other sections that arent implemented

--- frontend/UI.py
+++ frontend/UI.py
@@ -636,7 +636,7 @@ class Page4(Page):
 
         #Generate Renders Button
         self.GenerateRenders_Button = QPushButton('Generate Renders', self)
-        self.GenerateRenders_Button.clicked.connect(self.generateRandom)
+        self.GenerateRenders_Button.clicked.connect(self.generate_render)
         self.GenerateRenders_Button.setGeometry(0, 10, 125, 50)
 
         
@@ -663,18 +663,21 @@ class Page4(Page):
         # X Degree
         self.X_Degree_Label = QLabel("X:", self)
         self.X_Degree_input_field = QLineEdit(parent=self)
+        self.X_Degree_input_field.setText("1")
         self.X_Degree_slider = QtWidgets.QSlider(self)
         self.X_Degree_slider.setOrientation(QtCore.Qt.Horizontal)
 
         # Y Degree
         self.Y_Degree_Label = QLabel("Y:", self)
         self.Y_Degree_input_field = QLineEdit(parent=self)
+        self.Y_Degree_input_field.setText("1")
         self.Y_Degree_slider = QtWidgets.QSlider(self)
         self.Y_Degree_slider.setOrientation(QtCore.Qt.Horizontal)
 
         # Z Degree
         self.Z_Degree_Label = QLabel("Z:", self)
         self.Z_Degree_input_field = QLineEdit(parent=self)
+        self.Z_Degree_input_field.setText("1")
         self.Z_Degree_slider = QtWidgets.QSlider(self)
         self.Z_Degree_slider.setOrientation(QtCore.Qt.Horizontal)
     
@@ -725,21 +728,84 @@ class Page4(Page):
         super().resizeEvent(event)  # Call the parent class's resizeEvent
 
 
-    def add_camera_poses_linear(self):
+    def add_camera_poses_linearLEGACY(self):
         number_of_renders = int(self.Number_of_renders_input_field.text())
 
         x_degree_change = int(self.X_Degree_input_field.text())
         z_degree_change = int(self.Z_Degree_input_field.text())
         y_degree_change = int(self.Y_Degree_input_field.text())
 
-    def add_camera_poses_random():
-        pass
+        x_degree = 90
+        z_degree = 0
+        y_degree = 0
+
+        for i in range(0, number_of_renders):
+            x_degree += x_degree_change
+            z_degree += z_degree_change
+            y_degree += y_degree_change
 
 
-    def generateRandom(self):
+            print([np.deg2rad(x_degree), np.deg2rad(z_degree), np.deg2rad(y_degree)])
+            backend.add_cam_pose([[0, -5, 0],[np.deg2rad(x_degree), np.deg2rad(z_degree), np.deg2rad(y_degree)]])
+
+    def calculate_position(self, angle, distance):
+        #calculate x/z based on y
+        #caluclate y based on x
+        #z is a gangsta
+
+        r = np.sin(angle[0]) * distance
+
+        x_position = r * np.sin( angle[2] )   
+        z_position = -1 * r * np.cos( angle[2] )
+
+        y_position = np.cos(angle[0]) * distance
+
+        return [x_position, z_position, y_position]
+
+
+
+    def add_camera_poses_linear(self, pivot, distance_from_pivot):
+        #print(starting[0])
+        #working around 0 0 0 and pi/2 0 0  for now and distance of 5 
+
+        number_of_renders = int(self.Number_of_renders_input_field.text())        
+
+        x_change_angle = -1 * np.deg2rad( int(self.X_Degree_input_field.text()) )
+        starting_x_angle = pivot[1][0]
+
+        z_change_angle = np.deg2rad( int(self.Z_Degree_input_field.text()) )
+        starting_z_angle = pivot[1][1]
+
+        y_change_angle = np.deg2rad( int(self.Y_Degree_input_field.text()) )
+        starting_y_angle = pivot[1][2]
+
+
+
+        current_x_angle = starting_x_angle
+        current_z_angle = starting_z_angle
+        current_y_angle = starting_y_angle
+
+        for i in range(number_of_renders):
+            #Y CHANGE
+            camera_rotation = [current_x_angle,current_z_angle,current_y_angle]
+            #calculate position based on angle
+
+            position = self.calculate_position(camera_rotation, distance_from_pivot)
+
+            backend.add_cam_pose([position, camera_rotation])
+            print([position, camera_rotation])
+            #increment
+
+
+            current_x_angle += x_change_angle
+            current_z_angle += z_change_angle
+            current_y_angle += y_change_angle
+            
+
+        
+    def generate_render(self):
         #validate
         number_of_renders = int(self.Number_of_renders_input_field.text())
-
         if number_of_renders <1:
             QMessageBox.warning(self, "Error when starting render", "Invalid value for number of renders.")
             return
@@ -747,13 +813,13 @@ class Page4(Page):
         #add cameras
         
         #for now all cameras are linear
-
-        self.add_camera_poses_linear()
+        starting = [[0,0,0],[np.pi/2, 0 ,0]]
+        self.add_camera_poses_linear(starting, 5)  
         
 
 
         
-        #backend.render()
+        backend.render()
 
 
 
